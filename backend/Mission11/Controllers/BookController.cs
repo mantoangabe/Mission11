8d58867b0b651f7357847df5008394e2c201ec23
mantoangabe <mantoangabe@gmail.com>
2026-03-19 19:01:19 -0600

    Initial commit

---
+++ backend/Mission11/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission11.Models;
+
+namespace Mission11.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : ControllerBase
+{
+    public BookstoreContext _bookstoreContext;
+    
+    public BookController(BookstoreContext tempContext)
+    {
+        _bookstoreContext = tempContext;
+    }
+
+    [HttpGet]
+    public IActionResult GetBooks(int pageSize = 10, int pageNumber = 1)
+    {
+        var books = _bookstoreContext.Books.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+        var totalCount = _bookstoreContext.Books.Count();
+        return Ok(new
+            {
+        Books = books,
+        TotalCount = totalCount});
+}
+}
---
+++ backend/Mission11/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission11.Models;
+
+public partial class Book
+{
+    [Key]
+    public int BookId { get; set; }
+    [Required]
+    public string Title { get; set; }
+    public string? Author { get; set; }
+    public string? Publisher { get; set; }
+    public string? Isbn { get; set; }
+    public string? Classification { get; set; }
+    public string? Category { get; set; }
+    public int? PageCount { get; set; }
+    public double? Price { get; set; }
+}
