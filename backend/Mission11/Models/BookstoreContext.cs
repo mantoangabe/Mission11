a72b251e235ba67b0cf3d43da32ef23c0d2e8f01
mantoangabe <mantoangabe@gmail.com>
2026-04-02 00:23:06 -0600

    backend

--- backend/Mission11/Models/BookstoreContext.cs
+++ backend/Mission11/Models/BookstoreContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mission11.Models;
@@ -16,10 +15,6 @@ public partial class BookstoreContext : DbContext
     }
 
     public virtual DbSet<Book> Books { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=Data/Bookstore.sqlite");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- backend/Mission11/obj/Debug/net8.0/Mission11.AssemblyInfo.cs
+++ backend/Mission11/obj/Debug/net8.0/Mission11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d195c60cfb33c51c0be7cf62006b0d942478f4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5778d5d5527566784c17c2c5c07f15c4c0f67fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
